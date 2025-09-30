9f1a396c21b602b9dbdc015e02805af19bd69d18
Thiago Ritcher <thiagoritcher@gmail.com>
2025-09-29 22:17:13 -0300

    fix segfault

--- deps/src/main.zig
+++ deps/src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const deps = @import("deps");
 const zbench = @import("zbench");
 
-pub fn addx(a:i32, b:i32) i32{
+pub fn addx(a: i32, b: i32) i32 {
     return a + b;
 }
 
@@ -13,63 +13,50 @@ pub fn main() !void {
     var stdout = std.fs.File.stdout().writerStreaming(&.{});
     const writer = &stdout.interface;
     const alloc = std.heap.page_allocator;
-    
-//    const fmem = try aloc.alloc(Adder, 1);
-//    defer aloc.free(fmem);
-//    fmem = addx;
 
     var bench = zbench.Benchmark.init(alloc, .{});
     defer bench.deinit();
 
-//    const mfp =  MyBenchFP.init(addx);
-//    mfp.run(std.heap.page_allocator);
-
-    try bench.addParam("My Benchmark Static", &MyBenchS.init(), .{});
-    try bench.addParam("My Benchmark FP", &MyBenchFP.init(addx), .{});
+    try bench.add("My Benchmark FP", benchFP, .{});
+    try bench.add("My Benchmark Static", benchS, .{});
 
     try writer.writeAll("\n");
     try bench.run(writer);
 }
 
-const Adder = fn(i32,i32) i32;
+const Adder = *const fn (i32, i32) i32;
 
+pub fn benchFP(_: std.mem.Allocator) void {
+    const self = MyBenchFP.init(&addx);
+    _ = self.add(45, 55);
+}
+
+const MyBenchFP = struct {
+    addf: Adder,
+
+    fn init(comptime addfn: Adder) @This() {
+        return .{ .addf = addfn };
+    }
+
+    fn add(self: @This(), a: i32, b: i32) i32 {
+        return self.addf(a, b);
+    }
+};
+
+pub fn benchS(_: std.mem.Allocator) void {
+    const self = MyBenchS.init();
+    _ = self.add(45, 55);
+}
 
 const MyBenchS = struct {
     fn init() @This() {
         return .{};
     }
 
-    pub fn add(_: @This(), a:i32, b:i32) i32{
+    pub fn add(_: @This(), a: i32, b: i32) i32 {
         return a + b;
     }
-
-    pub fn run (self: @This(), _: std.mem.Allocator) void {
-        for (0..1000) |_| {
-            _ = self.add(45, 55);
-        }
-    }
 };
-
-const MyBenchFP = struct {
-    addf:*const Adder,
-
-    fn init(addfn: *const Adder) @This() {
-        return .{.addf = addfn};
-    }
-    
-    pub fn add(self: @This(), a:i32, b:i32) i32{
-        const f = self.addf;
-        return f(a,b);
-    }
-
-    pub fn run (self:@This(), _: std.mem.Allocator) void {
-        for (0..1000) |_| {
-            _ = self.add(45, 55);
-        }
-
-    }
-};
-
 
 test "simple test" {
     const gpa = std.testing.allocator;
